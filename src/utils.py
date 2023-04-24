8a14025fbc356c530209705001efa589387ae6f7
Deepak Singh <deepakvbps6@gmail.com>
2023-04-25 01:13:38 +0530

    Model trainer

--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,8 @@ import sys
 import pickle 
 import numpy as np
 import pandas as pd
+from src.exception import CustomException
+from src.logger import logging
 from sklearn.metrics import r2_score, mean_absolute_error,mean_squared_error
 
 def save_object(file_path, obj):
